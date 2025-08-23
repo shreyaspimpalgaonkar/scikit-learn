7bf8cf8b2906a91f15d56e7ad2202963cf03176b
abcds07 <abhi0709singh@gmail.com>
2025-08-24 04:09:24 +0530

    deleted loss variable

--- examples/linear_model/plot_sgd_loss_functions.py
+++ examples/linear_model/plot_sgd_loss_functions.py
@@ -17,9 +17,7 @@ import numpy as np
 
 def modified_huber_loss(y_true, y_pred):
     z = y_pred * y_true
-    loss = -4 * z
-    loss[z >= -1] = (1 - z[z >= -1]) ** 2
-    loss[z >= 1.0] = 0
+
     return loss
 
 
